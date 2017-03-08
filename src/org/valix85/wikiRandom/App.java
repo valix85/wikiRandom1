87cd9d05a1fa1bdeec07f33d0d4d93f0e8f1cb39
valix85 <valix85@gmail.com>
2017-03-08 09:45:26 +0100

    first commit

---
+++ src/org/valix85/wikiRandom/App.java
@@ -0,0 +1,11 @@
+package org.valix85.wikiRandom;
+
+/**
+ * Created by Valerio on 08/03/2017.
+ */
+public class App {
+    public static void main(String args[]){
+        //TODO...
+        System.out.println("First commit");
+    }
+}
